79918712c0a834f7c5547e10dee258bb913d5d90
Marta Garcia <marta.garcia@itequia.com>
2018-01-19 11:47:56 +0100

    commit luis dialog

---
+++ Bot Application/Dialogs/DefaultDialog .cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace Bot_Application.Dialogs
+{
+    [Serializable]
+    [LuisModel("66e2cca5-362c-42c4-81ef-6e3d2a95e6df","58be75eb94384005bf644f843e718ded")]
+    public class DefaultDialog: LuisDialog<object>
+    {
+    }
+}
