f5afb84f8ad08fd8546ad38b4bde9c97c911e6f1
Marta Garcia <marta.garcia@itequia.com>
2018-01-19 13:10:20 +0100

    commit root dialog

--- Bot Application/Dialogs/RootDialog.cs
+++ Bot Application/Dialogs/RootDialog.cs
@@ -12,35 +12,13 @@ namespace Bot_Application.Dialogs
     [LuisModel("66e2cca5-362c-42c4-81ef-6e3d2a95e6df", "58be75eb94384005bf644f843e718ded")]
     public class RootDialog : LuisDialog<object>
     {
-        //public Task StartAsync(IDialogContext context)
-        //{
-        //    context.Wait(MessageReceivedAsync);
-
-        //    return Task.CompletedTask;
-        //}
-
-        //private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
-        //{
-        //    var activity = await result as Activity;
-
-        //    // calculate something for us to return
-        //    int length = (activity.Text ?? string.Empty).Length;
-
-        //    // return our reply to the user
-        //    await context.PostAsync($"You sent {activity.Text} which was {length} characters");
-
-        //    context.Wait(MessageReceivedAsync);
-        //}
-
     
         [LuisIntent("")]
         [LuisIntent("None")]
         public async Task None(IDialogContext context, LuisResult result)
         {
             string message = $"Lo siento no te entiendo.'{result.Query}'. Escribe 'help' o 'ayuda' si necesitas ayuda.";
-
             await context.PostAsync(message);
-
             context.Wait(this.MessageReceived);
         }
 
@@ -54,10 +32,8 @@ namespace Bot_Application.Dialogs
         [LuisIntent("search-gif")]
         public async Task SearchGif(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
         {
-
             var message = await activity;
-            await context.PostAsync($"Bienvenido al buscador de gifts! Estamos analizando tu mensaje: '{message.Text}'...");
-
+            await context.PostAsync($"Aquí tienes tu gif: '{message.Text}'...");
         }
 
         [LuisIntent("help")]
@@ -67,7 +43,6 @@ namespace Bot_Application.Dialogs
 
             context.Wait(this.MessageReceived);
         }
-
 
         [LuisIntent("insulto")]
         public async Task Insulto(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
